6a67c180946b08ae8322077fd620edd144b73755
ngrokadmin <105307065+ngrokadmin@users.noreply.github.com>
2022-05-12 09:17:29 +0000

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,9 +8,9 @@ docker run --name cutebaby \
 -d -p 8080:9870 \
 okayandy/ubuntu-novnc:latest
 clear
-echo DOGE TO THE MOON :))
-echo Done! NoMachine Information:
-echo Port: 8080
-echo Pass: 66666
+echo "DOGE TO THE MOON ^^!"
+echo "Done! NoMachine Information:"
+echo "Port: 8080"
+echo "Pass: 66666"
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
